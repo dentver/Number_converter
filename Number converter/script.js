dcc4765b8c8559d1b77f7435916c5e69fbd641aa
dentver <zirkovd4@gmail.com>
2023-08-06 17:46:59 +0300

    add html and css file

---
+++ Number converter/script.js
@@ -0,0 +1,11 @@
+const convertButton = document.querySelector ('buttonConvert');
+
+let numberValue = document.getElementsByTagName("input")[0].value;
+
+convertButton.addEventListener("click", Convert);
+
+function Convert(){
+    console.log(numberValue);
+    return;
+}
+console. log("Какая-либо информация")
